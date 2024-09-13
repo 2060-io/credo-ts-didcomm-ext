d752a6041c399951e7cb793bff480f93e72c3690
Ariel Gentile <gentilester@gmail.com>
2024-09-13 17:59:19 -0300

    feat: initial import from 2060-mobile-agent
    Signed-off-by: Ariel Gentile <gentilester@gmail.com>

---
+++ packages/calls/tests/DidCommCallsService.test.ts
@@ -0,0 +1,42 @@
+import type { DidCommCallsService } from '../src/DidCommCallsService'
+import type { Agent } from '@credo-ts/core'
+
+import { JsonTransformer } from '@credo-ts/core'
+
+import { setupAgent } from './utils/agent'
+
+describe('Didcomm Calls', () => {
+  let agent: Agent
+  let didcommCallsService: DidCommCallsService
+
+  beforeAll(async () => {
+    agent = await setupAgent({
+      name: 'calls service test',
+    })
+    didcommCallsService = agent.dependencyManager.resolve(didcommCallsService)
+  })
+
+  afterAll(async () => {
+    await agent.shutdown()
+    await agent.wallet.delete()
+  })
+
+  describe('Accept message', () => {
+    test('Should create a valid https://didcomm.org/calls/1.0/call-accept message ', async () => {
+      const message = didcommCallsService.createAccept({
+        threadId: '5678-5678-5678-5678',
+        parameters: { param: 'value' },
+      })
+
+      const jsonMessage = JsonTransformer.toJSON(message)
+
+      expect(jsonMessage).toEqual(
+        expect.objectContaining({
+          '@id': expect.any(String),
+          '@type': 'https://didcomm.org/calls/1.0/call-accept',
+          parameters: { param: 'value', '~thread': expect.objectContaining({ thid: '5678-5678-5678-5678' }) },
+        }),
+      )
+    })
+  })
+})
