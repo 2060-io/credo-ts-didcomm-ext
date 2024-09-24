98cb5e6d970aadea5521cfc1450cff6d9c6a1748
Ariel Gentile <gentilester@gmail.com>
2024-09-24 10:17:56 -0300

    fix: expose DidCommCallType

--- packages/calls/src/messages/index.ts
+++ packages/calls/src/messages/index.ts
@@ -1,4 +1,4 @@
-export { CallOfferMessage } from './CallOfferMessage'
+export { CallOfferMessage, DidCommCallType } from './CallOfferMessage'
 export { CallAcceptMessage } from './CallAcceptMessage'
 export { CallRejectMessage } from './CallRejectMessage'
 export { CallEndMessage } from './CallEndMessage'
