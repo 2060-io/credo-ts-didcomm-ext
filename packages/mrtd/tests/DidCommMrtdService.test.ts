9089fbc474bf4356bd666b08cf24e6526943c26a
Andres Vallecilla <andresfelipe083195@hotmail.com>
2024-10-11 19:11:21 -0500

    feat: testing

--- packages/mrtd/tests/DidCommMrtdService.test.ts
+++ packages/mrtd/tests/DidCommMrtdService.test.ts
@@ -41,4 +41,36 @@ describe('Didcomm MRTD', () => {
       )
     })
   })
+
+  describe('EMRTD Data message', () => {
+    test('Should create a valid https://didcomm.org/mrtd/1.0/emrtd-data message ', async () => {
+      const message = didcommMrtdService.createEMrtdData({
+        threadId: '5678-5678-5678-5678',
+        dataGroups: {
+          COM: 'Something',
+          DG1: 'Something',
+          DG2: 'Something',
+          DG11: 'Something',
+          SOD: 'Something',
+        },
+      })
+
+      const jsonMessage = JsonTransformer.toJSON(message)
+
+      expect(jsonMessage).toEqual(
+        expect.objectContaining({
+          '@id': expect.any(String),
+          '@type': 'https://didcomm.org/mrtd/1.0/emrtd-data',
+          dataGroups: {
+            COM: expect.any(String),
+            DG1: expect.any(String),
+            DG2: expect.any(String),
+            DG11: expect.any(String),
+            SOD: expect.any(String),
+          },
+          '~thread': expect.objectContaining({ thid: '5678-5678-5678-5678' }),
+        }),
+      )
+    })
+  })
 })
