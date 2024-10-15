04600348937a643c69edfbff67398d29ca436ca1
Andres Vallecilla <andresfelipe083195@hotmail.com>
2024-10-15 13:17:55 -0500

    fix: fix load params

--- packages/mrtd/src/esm/index.ts
+++ packages/mrtd/src/esm/index.ts
@@ -1,3 +1,3 @@
-import * as tsemrtd from '@li0ard/tsemrtd'
+import { COM, DG1, DG2, DG3, DG4, DG5, DG7, DG11, DG12, DG14, DG15, SOD, PKD } from '@li0ard/tsemrtd'
 
-export { tsemrtd }
+export { COM, DG1, DG2, DG3, DG4, DG5, DG7, DG11, DG12, DG14, DG15, SOD, PKD }
--- packages/mrtd/src/models/EMrtdData.ts
+++ packages/mrtd/src/models/EMrtdData.ts
@@ -1,4 +1,4 @@
-import { tsemrtd } from '../esm'
+import { COM, DG1, DG2, DG3, DG4, DG5, DG7, DG11, DG12, DG14, DG15, SOD, PKD } from '../esm'
 
 import { EMrtdDataGroup } from './EMrtdDataGroup'
 
@@ -13,45 +13,46 @@ export function EMrtdParse(input: Record<string, string>): ParsedEMrtdData {
   const parsedData: ParsedEMrtdData = {}
 
   for (const [key, value] of Object.entries(input)) {
+    const decodedValue = Buffer.from(value, 'base64')
     switch (key as EMrtdDataGroup) {
       case EMrtdDataGroup.COM:
-        parsedData[EMrtdDataGroup.COM] = JSON.stringify(tsemrtd.COM.load(value))
+        parsedData[EMrtdDataGroup.COM] = JSON.stringify(COM.load(decodedValue))
         break
       case EMrtdDataGroup.DG1:
-        parsedData[EMrtdDataGroup.DG1] = JSON.stringify(tsemrtd.DG1.load(value))
+        parsedData[EMrtdDataGroup.DG1] = JSON.stringify(DG1.load(decodedValue))
         break
       case EMrtdDataGroup.DG2:
-        parsedData[EMrtdDataGroup.DG2] = JSON.stringify(tsemrtd.DG2.load(value))
+        parsedData[EMrtdDataGroup.DG2] = JSON.stringify(DG2.load(decodedValue))
         break
       case EMrtdDataGroup.DG3:
-        parsedData[EMrtdDataGroup.DG3] = JSON.stringify(tsemrtd.DG3.load(value))
+        parsedData[EMrtdDataGroup.DG3] = JSON.stringify(DG3.load(decodedValue))
         break
       case EMrtdDataGroup.DG4:
-        parsedData[EMrtdDataGroup.DG4] = JSON.stringify(tsemrtd.DG4.load(value))
+        parsedData[EMrtdDataGroup.DG4] = JSON.stringify(DG4.load(decodedValue))
         break
       case EMrtdDataGroup.DG5:
-        parsedData[EMrtdDataGroup.DG5] = JSON.stringify(tsemrtd.DG5.load(value))
+        parsedData[EMrtdDataGroup.DG5] = JSON.stringify(DG5.load(decodedValue))
         break
       case EMrtdDataGroup.DG7:
-        parsedData[EMrtdDataGroup.DG7] = JSON.stringify(tsemrtd.DG7.load(value))
+        parsedData[EMrtdDataGroup.DG7] = JSON.stringify(DG7.load(decodedValue))
         break
       case EMrtdDataGroup.DG11:
-        parsedData[EMrtdDataGroup.DG11] = JSON.stringify(tsemrtd.DG11.load(value))
+        parsedData[EMrtdDataGroup.DG11] = JSON.stringify(DG11.load(decodedValue))
         break
       case EMrtdDataGroup.DG12:
-        parsedData[EMrtdDataGroup.DG12] = JSON.stringify(tsemrtd.DG12.load(value))
+        parsedData[EMrtdDataGroup.DG12] = JSON.stringify(DG12.load(decodedValue))
         break
       case EMrtdDataGroup.DG14:
-        parsedData[EMrtdDataGroup.DG14] = JSON.stringify(tsemrtd.DG14.load(value))
+        parsedData[EMrtdDataGroup.DG14] = JSON.stringify(DG14.load(decodedValue))
         break
       case EMrtdDataGroup.DG15:
-        parsedData[EMrtdDataGroup.DG15] = JSON.stringify(tsemrtd.DG15.load(value))
+        parsedData[EMrtdDataGroup.DG15] = JSON.stringify(DG15.load(decodedValue))
         break
       case EMrtdDataGroup.SOD:
-        parsedData[EMrtdDataGroup.SOD] = JSON.stringify(tsemrtd.SOD.load(value))
+        parsedData[EMrtdDataGroup.SOD] = JSON.stringify(SOD.load(decodedValue))
         break
       case EMrtdDataGroup.PKD:
-        parsedData[EMrtdDataGroup.PKD] = JSON.stringify(tsemrtd.PKD.load(value))
+        parsedData[EMrtdDataGroup.PKD] = JSON.stringify(PKD.load(decodedValue))
         break
       default:
         break
