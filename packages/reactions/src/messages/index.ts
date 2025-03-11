bd2507c992bd560eaeecdcc984038ad2418e2ccb
Ariel Gentile <gentilester@gmail.com>
2025-03-11 18:23:01 -0300

    Update packages/reactions/src/messages/index.ts
    Co-authored-by: andresfv95 <andresfelipe083195@hotmail.com>

--- packages/reactions/src/messages/index.ts
+++ packages/reactions/src/messages/index.ts
@@ -1,2 +1,2 @@
-export { MessageReaction, MessageReactionsMessage } from './MessageReactionsMessage'
+export { MessageReaction, MessageReactionsMessage, MessageReactionAction } from './MessageReactionsMessage'
 export type { MessageReactionsMessageOptions } from './MessageReactionsMessage'
