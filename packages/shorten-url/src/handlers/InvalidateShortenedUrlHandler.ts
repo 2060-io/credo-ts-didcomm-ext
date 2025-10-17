06c1cc9746e7efebf75cbc6eff41012dac7282dc
Gabriel Mata <gabrielmatau@gmail.com>
2025-10-17 12:02:44 -0500

    fix: Fix import exposed
    Signed-off-by: Gabriel Mata <gabrielmatau@gmail.com>

--- packages/shorten-url/src/handlers/InvalidateShortenedUrlHandler.ts
+++ packages/shorten-url/src/handlers/InvalidateShortenedUrlHandler.ts
@@ -2,7 +2,7 @@ import type { DidCommShortenUrlService } from '../DidCommShortenUrlService'
 import type { MessageHandler, MessageHandlerInboundMessage } from '@credo-ts/core'
 
 import { getOutboundMessageContext } from '@credo-ts/core'
-import { AckMessage, AckStatus } from '@credo-ts/core/build/modules/common/messages/AckMessage'
+import { AckMessage, AckStatus } from '@credo-ts/core'
 
 import { InvalidateShortenedUrlMessage } from '../messages'
 
